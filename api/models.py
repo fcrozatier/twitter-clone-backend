8e9f6bd71405d835174c0e02bd5570b31cfb374b
fcrozatier <frederic.crozatier@protonmail.com>
2021-05-21 16:45:00 +0200

    create models for Nodes and Relations

--- api/models.py
+++ api/models.py
@@ -1,12 +1,38 @@
-from neomodel import DateTimeProperty, StringProperty, StructuredNode, UniqueIdProperty
+from neomodel import (
+    DateTimeProperty,
+    RelationshipTo,
+    StringProperty,
+    StructuredNode,
+    StructuredRel,
+    UniqueIdProperty,
+)
 
 
-class User(StructuredNode):
-    uid = UniqueIdProperty()
-    name = StringProperty(required=True)
+class TweetRel(StructuredRel):
+    date = DateTimeProperty(default_now=True)
+
+
+class ReTweetRel(StructuredRel):
+    date = DateTimeProperty(default_now=True)
 
 
 class Post(StructuredNode):
     uid = UniqueIdProperty()
     content = StringProperty(required=True)
     created = DateTimeProperty(default_now=True)
+
+
+class Comment(StructuredNode):
+    uid = UniqueIdProperty()
+    content = StringProperty(required=True)
+    created = DateTimeProperty(default_now=True)
+    comments = RelationshipTo(Post, "COMMENTS")
+
+
+class User(StructuredNode):
+    uid = UniqueIdProperty()
+    name = StringProperty(required=True)
+    tweets = RelationshipTo(Post, "TWEETS", model=TweetRel)
+    retweets = RelationshipTo(Post, "RETWEETS", model=ReTweetRel)
+    likes = RelationshipTo(Post, "LIKES")
+    reactions = RelationshipTo(Comment, "REACTION")
