71e98d1dc48bd1deaa58b2536fb47381632f0478
fcrozatier <frederic.crozatier@protonmail.com>
2021-06-07 16:32:16 +0200

    customize admin site for user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
 
+from accounts.forms import CreateUserForm
 from accounts.models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = ("username", "email", "uid", "is_staff")
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("username", "email")
+    ordering = ("username",)
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+    add_form = CreateUserForm
