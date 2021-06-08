76db842b087aa66ea475639cd9f22d388d2ece94
fcrozatier <frederic.crozatier@protonmail.com>
2021-06-08 11:27:42 +0200

    improve admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,19 +1,31 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import gettext_lazy as _
+from graphql_auth.models import UserStatus
 
 from accounts.forms import CreateUserForm
 from accounts.models import User
 
+admin.site.register(UserStatus)
+
 
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
-    list_display = ("username", "email", "uid", "is_staff")
-    list_filter = ("is_staff", "is_active")
+    list_display = ("username", "email", "verified", "uid", "is_staff")
+    list_filter = ("is_staff", "is_active", "status__verified")
     search_fields = ("username", "email")
     ordering = ("username",)
     fieldsets = (
-        (None, {"fields": ("email", "username", "password")}),
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "username",
+                    "password",
+                )
+            },
+        ),
         (
             _("Permissions"),
             {
@@ -36,3 +48,8 @@ class UserAdmin(DjangoUserAdmin):
         ),
     )
     add_form = CreateUserForm
+    filter_horizontal = ()
+
+    @admin.display(boolean=True)
+    def verified(self, user):
+        return user.status.verified
