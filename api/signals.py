9fe4736187d56657955e73fa69074586366e5c77
fcrozatier <frederic.crozatier@protonmail.com>
2021-06-08 18:30:37 +0200

    remove print statement from signals

--- api/signals.py
+++ api/signals.py
@@ -6,5 +6,4 @@ from api.models import UserNode
 
 @receiver(user_registered)
 def create_user_node(sender, user, **kwargs):
-    print("A User has been registered !")
     UserNode(uid=user.uid).save()
