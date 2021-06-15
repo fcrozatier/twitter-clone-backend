02d56b4fd60f368d8b8be3b84e47679db5a5c951
fcrozatier <frederic.crozatier@protonmail.com>
2021-06-15 14:42:01 +0200

    rename not likeable error

--- api/errors.py
+++ api/errors.py
@@ -1,4 +1,5 @@
 TWEET_NOT_FOUND_ERROR = "Tweet not found"
 TWEET_EMPTY_ERROR = "Your tweet is empty"
 COMMENT_EMPTY_ERROR = "Your comment is empty"
+NOT_LIKEABLE = "This cannot be liked"
 GENERIC_ERROR = "An error occured"
