934ec7ca50dcf2ee3b0ad293b1440198815ac84a
fcrozatier <frederic.crozatier@protonmail.com>
2021-06-16 17:29:20 +0200

    create UserType

--- api/schema/types.py
+++ api/schema/types.py
@@ -1,4 +1,5 @@
 import graphene
+from accounts.models import User
 from api.schema.decorators import add_base_resolvers
 from graphene.types.objecttype import ObjectType
 
@@ -61,3 +62,33 @@ class ReTweetType(ObjectType):
 
     def resolve_comments_list(parent, info):
         return parent.commented.all()
+
+
+@add_base_resolvers
+class UserType(ObjectType):
+    uid = graphene.String(required=True)
+    email = graphene.String()
+    username = graphene.String()
+    followers_count = graphene.Int()
+    tweets = graphene.List(TweetType)
+    retweets = graphene.List(ReTweetType)
+    comments = graphene.List(CommentType)
+    likes = graphene.List(LikeableType)
+
+    def resolve_email(parent, info):
+        return User.objects.get(uid=parent.uid).email
+
+    def resolve_username(parent, info):
+        return User.objects.get(uid=parent.uid).username
+
+    def resolve_tweets(parent, info):
+        return parent.tweets.all()
+
+    def resolve_retweets(parent, info):
+        return parent.retweets.all()
+
+    def resolve_comments(parent, info):
+        return parent.comments.all()
+
+    def resolve_likes(parent, info):
+        return parent.likes.all()
